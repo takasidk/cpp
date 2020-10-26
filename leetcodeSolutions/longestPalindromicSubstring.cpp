180f38662776c6069158ba9aee508f350e8adcd4
takasidk <57638212+takasidk@users.noreply.github.com>
2020-10-26 22:15:19 +0530

    schanges

--- leetcodeSolutions/longestPalindromicSubstring.cpp
+++ leetcodeSolutions/longestPalindromicSubstring.cpp
@@ -17,8 +17,8 @@ Constraints:
 1 <= s.length <= 1000
 s consist of only digits and English letters (lower-case and/or upper-case),*/
 class Solution {
-    int max_len=1;
-        int st=0,end=0;
+    int max_len=1;   //global variables for this class
+      int st=0,end=0;
     void getLen(string s,int l,int r){
         int len=1;
         while(l>=0 && r<s.length()){
@@ -36,14 +36,14 @@ class Solution {
             end=r-1;}
     }
 public:
-    string longestPalindrome(string s) {
+    string longestPalindrome(string s) {//for odd and even length substring the traversals will be diff
         int n=s.length();
-        if(n<=1) return s;
+        if(n<=1) return s; // if length of string less then 1 return string itself
         for(int i=0;i<n;i++){
             int l=i,r=i;
-            getLen(s,l,r);
-            getLen(s,l,r+1);
+            getLen(s,l,r);//for odd length substring ex:"cbabad" here ans is "bab"odd length 
+            getLen(s,l,r+1);//for even length substring ex:"abb" here ans:"bb" even length
         }
-        return s.substr(st,max_len);
+        return s.substr(st,max_len);//inbuilt substring fn which takes start pos and length of substring
     }
 };
