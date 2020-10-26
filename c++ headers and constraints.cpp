b92132b692a78c1b10e060f1e32ce2d72b9b2ac2
takasidk <57638212+takasidk@users.noreply.github.com>
2020-10-26 20:41:00 +0530

    first commit

---
+++ c++ headers and constraints.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// Numeric Constants
+#define MOD 1000000007
+#define maxs 250005
+#define mins 1005
+#define eps 0.000000000001
+#define pi 3.141592653589793
+ 
+// Others
+#define ll long long int
+#define pb push_back
+#define iosbase ios_base::sync_with_stdio(false)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define ppi pair<pair<int,int>,int>	
+#define ppl pair<pll,ll>
+#define vi vector<int>
+#define F first
+#define S second
+#define siter set<int>::iterator
+#define p_pq priority_queue
+#define ub upper_bound
+#define lb lower_bound
+//
+#include<bits/stdc++.h>
+using namespace std;
+const double PI = 3.14159265358979323846;
+#define ll unsigned long long int
+#define ld long double
+#define ms(dp,val) memset(dp,val,sizeof(dp))
+#define all(t) t.begin(), t.end()
+#define inrange(i, a, b) ((i >= min(a, b)) && (i <= max(a, b)))
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define mod 1000000007
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<pii> vpi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+#define ul unsigned long long int 
